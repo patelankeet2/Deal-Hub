80ae41afccd7e606a2119bbc48936f35c07266ae
patelankeet2 <patelankeet2@gmail.com>
2025-05-20 10:42:36 +1200

    Made some changes so that we can run and surf to different pages

--- dealhub/src/App.js
+++ dealhub/src/App.js
@@ -4,6 +4,9 @@ import Register from './components/Register';
 import LandingPage from './components/LandingPage';
 import MerchantLogin from './components/MerchantLogin';
 import MerchantDashboard from './components/MerchantDashboard';
+import CreateDeal from './components/CreateDeal';
+import AnalyticsPage from './components/AnalyticsPage';
+import Settings from './components/Settings';
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
         <Route path="/landing" element={<LandingPage />} />
         <Route path="/merchant-login" element={<MerchantLogin />} />
         <Route path="/merchant-dashboard" element={<MerchantDashboard/>} /> 
+        <Route path="/create-deal" element={<CreateDeal />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   );
