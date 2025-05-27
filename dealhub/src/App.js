b2bb60f1bdc5dba7ea073a700bad1ff312b4e97d
mazhar1341 <faizimazhar2@gmail.com>
2025-05-27 15:06:16 +1200

    updated app.js to add router for admin pages

--- dealhub/src/App.js
+++ dealhub/src/App.js
@@ -3,6 +3,11 @@ import Login from './components/Login';
 import Register from './components/Register';
 import LandingPage from './components/LandingPage';
 import MerchantLogin from './components/MerchantLogin';
+import AdminManageDeals from './components/AdminManageDeals';
+import AdminEarnings from './components/AdminEarnings';
+import AdminProfile from './components/AdminProfile';
+import AdminDashboard from './components/AdminDashboard';
+import AdminManageUsers from './components/AdminManageUsers';
 
 function App() {
   return (
@@ -13,6 +18,11 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<LandingPage />} />
         <Route path="/merchant-login" element={<MerchantLogin />} />
+        <Route path="/admin-manage-deals" element={<AdminManageDeals />} />
+        <Route path="/admin-earnings" element={<AdminEarnings />} />
+        <Route path="/admin-profile" element={<AdminProfile />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/admin-manage-users" element={<AdminManageUsers />} />
       </Routes>
     </Router>
   );
